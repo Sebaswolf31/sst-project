baf6600825b9d570832a498671a4f030aed82684
Sebaswolf31 <sebasunivercal@hotmail.com>
2025-04-22 17:52:19 -0500

    Configuracion TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,44 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
+import typeormConfig from './config/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
+
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES },
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const dbConfig = config.get('typeorm') || {};
+        console.log('📡 Intentando conectar a la base de datos...');
+        return dbConfig;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {}
+
+  onModuleInit() {
+    console.log('✅ Módulo iniciado correctamente.');
+    console.log(
+      '🔗 Configuración actual de la BD:',
+      this.configService.get('typeorm'),
+    );
+  }
+}
