b68a8915322759ef9526f2367169213e52ef8425
Sebaswolf31 <sebasunivercal@hotmail.com>
2025-04-24 18:44:32 -0500

    DTO creadas, risk, inspection y document

--- backend/src/document/dto/create-document.dto.ts
+++ backend/src/document/dto/create-document.dto.ts
@@ -1,1 +1,14 @@
-export class CreateDocumentDto {}
+import { IsUUID, IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateDocumentDto {
+  @IsString()
+  @IsNotEmpty()
+  filename: string;
+
+  @IsString()
+  @IsNotEmpty()
+  path: string;
+
+  @IsUUID()
+  companyId: string;
+}
