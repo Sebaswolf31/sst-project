5ec8b0ac6650f7d4351166a4f5266ab48aca11ee
pandreasantacruz <andreasantacruz123@gmail.com>
2025-04-24 21:35:17 -0500

    "Update userRegister"

--- frontend/src/app/superAdmin/users/page.tsx
+++ frontend/src/app/superAdmin/users/page.tsx
@@ -1,11 +1,20 @@
+"use client";
 import React from "react";
 import RegisterUser from "./registerUser";
+import Accordion from "@/app/components/accordion";
 
 const Users = () => {
   return (
     <div>
-      Users
-      <RegisterUser />
+      <div className="w-full p-4 rounded-t-xl">
+        <h1 className="text-3xl font-semibold text-center ">
+          Panel de Usuarios
+        </h1>
+        <hr className="w-full my-2 border-t-2 border-greenP" />
+      </div>
+      <Accordion title="Registro de usuarios por empresa contratante">
+        <RegisterUser />
+      </Accordion>
     </div>
   );
 };
--- frontend/src/app/superAdmin/users/registerUser.tsx
+++ frontend/src/app/superAdmin/users/registerUser.tsx
@@ -70,7 +70,7 @@ const RegisterUser = () => {
   return (
     <div className="flex items-center justify-center min-h-screen p-4 bg-gray-100">
       <div className="w-full max-w-lg p-8 bg-white shadow-lg rounded-xl">
-        <h2 className="mb-8 text-3xl font-semibold text-center text-gray-800">
+        <h2 className="mb-8 text-2xl font-semibold text-center text-gray-800">
           Registro Usuarios
         </h2>
 
